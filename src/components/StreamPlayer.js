f60e3dceb14e11367258188056931a3808382b8e
Tung Pham <tung.42@gmail.com>
2025-02-10 14:00:50 +0700

    Update StreamPlayer.js

--- src/components/StreamPlayer.js
+++ src/components/StreamPlayer.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 import { Container, Button } from "react-bootstrap";
 
 const StreamPlayer = () => {
   const { id, season, episode } = useParams();
+  const [show, setShow] = useState([]);
   const embedUrl = `https://hoc.cotuong.top/se_player.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`;
+
+  const API_KEY = "fecb69b9d0ad64dbe0802939fafc338d"; // Replace with your TMDB API Key
+  const BASE_URL = "https://api.themoviedb.org/3";
+
+  useEffect(() => {
+    const fetchTVShow = async () => {
+      const response = await axios.get(`${BASE_URL}/tv/${id}`, {
+        params: { api_key: API_KEY, language: "vi" },
+      });
+      setShow(response.data);
+    };
+
+    fetchTVShow();
+  }, [id]);
 
   return (
     <Container className="mt-4 text-center">
       <h2 className="mb-3">
-        Chiếu Tập {episode} (Mùa {season})
+        Phim "{show.name}" Chiếu Tập {episode} (Mùa {season})
       </h2>
       <iframe
         src={embedUrl}
